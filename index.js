7a1641c0861b1606a11a4d630775302bddb6b9c5
rithvik <gadchandarithvikrao@gmail.com>
2023-05-26 20:00:28 +0530

    making final changes

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ fetch('https://www.coursehubiitg.in/api/codingweek/contributions')
         $(".one .round").text(data[0].name+'.'+data[0].points);
         $(".two .round").text(data[1].name+'.'+data[1].points);
         $(".three .round").text(data[2].name+'.'+data[2].points);
-        $(".one .main").html('<img height="100px" src="'+data[0].avatar+'"/>');
-        $(".two .sub").html('<img height="75px" src="'+data[1].avatar+'"/>');
-        $(".three .sub").html('<img height="75px" src="'+data[2].avatar+'"/>');
+        $(".one .main").html('<img height="150px" src="'+data[0].avatar+'"/>');
+        $(".two .sub").html('<img height="100px" src="'+data[1].avatar+'"/>');
+        $(".three .sub").html('<img height="100px" src="'+data[2].avatar+'"/>');
         for(var i=3;i<data.length;i++){
             const markup ='<div class="name"><div class="arrange"><p class="sno text">'+(i+1)+'</p><div class="scircle">'+'<img height="80px" src="'+data[i].avatar+'"/>'+'</div><p class="participant text">'+data[i].name+'</p></div><p class="score text">'+data[i].points+'</p></div>';
             document.querySelector('.cont').insertAdjacentHTML('beforeend',markup);
